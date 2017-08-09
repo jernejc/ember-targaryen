51bb543a2e0c1f3c87d4cb4ab9f1080dc82caeca
Jernej <j@eee.si>
2017-08-09 17:22:26 +0200

    new approach

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-/* eslint-env node */
-'use strict';
+var path = require('path');
+var mergeTrees = require('broccoli-merge-trees');
+var Funnel = require('broccoli-funnel');
 
 module.exports = {
-  name: 'targaryen'
+  name: 'targaryen',
+
+  treeForVendor: function(tree) {
+    var packagePath = path.dirname(require.resolve('targaryen'));
+    var packageTree = new Funnel(this.treeGenerator(packagePath), {
+      srcDir: '/',
+      destDir: 'targaryen'
+    });
+    
+    return mergeTrees([tree, packageTree]);
+  },
+
+  included: function(app) {
+    this._super.included(app);
+
+    if (app.import) {
+      this.importDependencies(app);
+    }
+  },
+
+  importDependencies: function(app) {
+    app.import('vendor/targaryen/index.js');
+  }
 };
